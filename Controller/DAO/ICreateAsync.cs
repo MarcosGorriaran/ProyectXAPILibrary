8a92e0e282769c1f1bd90d23c11d6b746ad24e5a
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2025-02-13 15:57:05 +0100

    Added interface ICreateAsync

---
+++ Controller/DAO/ICreateAsync.cs
@@ -0,0 +1,9 @@
+﻿using ProyectXAPI.Models;
+
+namespace ProyectXAPILibrary.Controller.DAO
+{
+    public interface ICreateAsync<T> where T : Model
+    {
+        public Task<Uri> CreateAsync(T modelElement);
+    }
+}
