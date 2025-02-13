c19568f9774e6b5bee859a7eab2d26acf9df3fe3
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2025-02-13 15:49:50 +0100

    Copied all the models from the API

---
+++ Models/DTO/ResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectXAPI.Models.DTO
+{
+    public class ResponseDTO
+    {
+        public object? Data { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string? Message { get; set; } = "";
+
+    }
+}
